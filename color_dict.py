b2dacc011ab30a863ac498f41b4758dc9e8a67ea
glenn mohre <glenn.mohre@gmail.com>
2017-02-23 12:52:54 -0500

    updated consequences and color_dict classes with comments and abstractions

--- color_dict.py
+++ color_dict.py
@@ -4,11 +4,14 @@ from collections import defaultdict
 class ColorDict(defaultdict):
     def __init__(self, c_dict=None):
         super(ColorDict, self).__init__(int)
+        # I'm tempted to remove this check and a default none,
+        # unless you're cool with empty colordicts
         if c_dict is not None:
             for k in c_dict:
                 self[k] = c_dict[k]
 
     def add_mana(self, c_dict):
+        # Adds c_dict to internal color dict
         for k in c_dict:
             self[k] += c_dict[k]
 
--- consequences.py
+++ consequences.py
@@ -1,27 +1,44 @@
 class Consequence(object):
+    '''
+    Base Consequence class, which is used for Action constructors.
+    '''
     def __init__(self):
         pass
+    def happen(selfself, game_state):
+        pass
 
-
-class ReduceMana(Consequence):
+class ManaConsequence(object):
+    '''
+    For Consequences that affect mana
+    '''
     def __init__(self, c_dict):
         self.c_dict = c_dict
-
+class ReduceMana(ManaConsequence):
+    '''
+    Pay Mana
+    '''
     def happen(self, game_state):
         game_state.reduce_mana(self.c_dict)
 
-
-class AddMana(Consequence):
-    def __init__(self, c_dict):
-        self.c_dict = c_dict
-
+class AddMana(ManaConsequence):
+    '''
+    Gain Mana
+    '''
     def happen(self, game_state):
         game_state.add_mana(self.c_dict)
-
-
-class MoveCard(Consequence):
-    def __init__(self, card, from_zone, to_zone):
+class CardConsequence(Consequence):
+    '''
+    Consequences for cards
+    '''
+    def __init__(self, card):
         self.card = card
+
+class MoveCard(CardConsequence):
+    '''
+    Moving a Card
+    '''
+    def __init__(self, card, from_zone, to_zone):
+        super(self, MoveCard).__init__(card)
         self.from_zone = from_zone
         self.to_zone = to_zone
 
@@ -29,14 +46,12 @@ class MoveCard(Consequence):
         game_state.reduce_card_count(self.card, self.from_zone)
         game_state.increase_card_count(self.card, self.to_zone)
 
-
-class Tap(Consequence):
-    def __init__(self, card):
-        self.card = card
-
+class Tap(CardConsequence):
+    '''
+    Tapping a Card
+    '''
     def happen(self, game_state):
         game_state.tap_card(self.card)
-
 
 class AddStorm(Consequence):
     def happen(self, game_state):
