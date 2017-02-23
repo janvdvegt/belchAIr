705076382ba7614c1c696a2741458b2e4f78e2fe
janvdvegt <jan.vegt@gmail.com>
2017-02-23 19:15:07 +0100

    First full card list

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 COLORS = ['Red', 'Green', 'Blue']
+CARDS_FOR_CHROME_MOX = ['Elvish Spirit Guide', 'Simian Spirit Guide',
+                        'Tinder Wall', 'Desperate Ritual', 'Pyretic Ritual',
+                        'Rite of Flame', 'Seething Song', 'Empty the Warrens',
+                        'Gitaxian Probe', 'Land Grant', 'Manamorphose',
+                        'Burning Wish', 'Reforge the Soul']
+CARDS_FOR_BURNING_WISH = ['Empty the Warrens', 'Reforge the Soul']
